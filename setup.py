aac424afa093cdc175d10e98dec0db0a06d20aa8
Yu-Cheng Chung <69280949+miles0428@users.noreply.github.com>
2025-08-02 22:39:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class PyTest(TestCommand):
 
 setup(
     name="DMET-nolab",
-    version="0.1.24",
+    version="0.1.25",
     packages=find_packages(),  # Automatically find all packages
     install_requires=[
         "numpy",
