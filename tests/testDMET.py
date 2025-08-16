31bbbed48962209ecc023e7631c268152e9dc431
Yu-Cheng Chung <tearofclown0428@gmail.com>
2025-08-16 19:15:26 +0800

    fix some bug

--- tests/testDMET.py
+++ tests/testDMET.py
@@ -11,14 +11,15 @@ L = 120
 t= 1.0
 U = 8.0
 number_of_electrons = 100
-fragment_size = 6  # Each fragment will have 2 orbitals
+fragment_size = 2  # Each fragment will have 2 orbitals
 fragments = [[ i+fragment_size*j for  i in range(fragment_size)]for j in range(L//fragment_size*2)]  # Create L/2 fragments, each with 2 orbitals
 
 # --- 建立 many-body problem ---
+one_body_hubbard = OneBodyHubbardFormulation(L=L, t=t, number_of_electrons=number_of_electrons)
 many_body_hubbard = ManyBodyHubbardFormulation(L=L, t=t, U=U)
 hubbard_problem = ProblemFormulation()
 hubbard_problem.one_body_problem_formulation = one_body_hubbard
 hubbard_problem.many_body_problem_formulation = many_body_hubbard
 problemsolver = EigenSolver()
-DMET_instance = DMET(hubbard_problem,fragments=fragments,problem_solver=problemsolver,verbose=True,PBC=True)
+DMET_instance = DMET(hubbard_problem,fragments=fragments,problem_solver=problemsolver,verbose=2,PBC=True)
 energy =  DMET_instance.run(0,-2)
