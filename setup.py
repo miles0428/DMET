60d7839426a30a5e2043692d89f2f43ab59b7ab5
Yu-Cheng Chung <tearofclown0428@gmail.com>
2025-09-20 16:04:53 +0800

    update filling order

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class PyTest(TestCommand):
 
 setup(
     name="DMET-nolab",
-    version="0.2.24",
+    version="0.2.25",
     packages=find_packages(),  # Automatically find all packages
     install_requires=[
         "numpy",
