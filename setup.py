802ac491f24647e095cc1812706b43a19ffa2f37
Yu-Cheng Chung <tearofclown0428@gmail.com>
2025-09-28 18:34:37 +0800

    update hf_update_rate

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class PyTest(TestCommand):
 
 setup(
     name="DMET-nolab",
-    version="0.2.27",
+    version="0.2.28",
     packages=find_packages(),  # Automatically find all packages
     install_requires=[
         "numpy",
