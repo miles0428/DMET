c55c702c9df158aec74e9043000977ed61f547a1
Yu-Cheng Chung <69280949+miles0428@users.noreply.github.com>
2026-02-26 14:13:37 +0800

    Bump version from 0.2.32 to 0.2.33

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class PyTest(TestCommand):
 
 setup(
     name="DMET-nolab",
-    version="0.2.32",
+    version="0.2.33",
     packages=find_packages(),  # Automatically find all packages
     install_requires=[
         "numpy",
