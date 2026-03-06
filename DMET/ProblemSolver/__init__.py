b8d3d0aaad470ea9e77dc067c416b4cc1d25d2d5
miles0428 <miles0428@users.noreply.github.com>
2026-03-06 20:44:17 +0000

    Move GPU solver under classical folder [by openclaw: vitamin]

--- DMET/ProblemSolver/__init__.py
+++ DMET/ProblemSolver/__init__.py
@@ -3,7 +3,7 @@ from .ClassicalEigenSolver.EigenSolver import EigenSolver
 from .QuantumEigenSolver.EigenSolver import EigenSolver
 # GPU solver (optional)
 try:
-    from .GpuEigenSolver import EigenSolver as GpuEigenSolver
+    from .ClassicalEigenSolver.GpuEigenSolver import EigenSolver as GpuEigenSolver
 except Exception:
     # If GPU solver cannot be imported (no cupy), ignore
     GpuEigenSolver = None
